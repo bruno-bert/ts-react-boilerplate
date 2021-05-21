f9667bc47a7b447ed27d375bfd031fb78367d081
Bruno de Paula <bruno.bert.jj@gmail.com>
2021-05-21 12:34:38 -0300

    feat: tewtwerwerwe

--- src/services/usecases/authentication/remote-authentication.ts
+++ src/services/usecases/authentication/remote-authentication.ts
@@ -3,11 +3,11 @@ import { HttpPostClient } from '@/services/interfaces/http-post-client'
 
 export class RemoteAuthentication {
   constructor (
-    private readonly url1: string,
+    private readonly url2: string,
     private readonly httpPostClient: HttpPostClient
   ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url1, body: params })
+    await this.httpPostClient.post({ url: this.url2, body: params })
   }
 }
