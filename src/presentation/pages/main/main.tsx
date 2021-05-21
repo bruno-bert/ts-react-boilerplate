95403ab0a3672d65fc7546be7053c849b2bf6e6d
Bruno de Paula <bruno.bert.jj@gmail.com>
2021-05-21 16:01:29 -0300

    chore: webpack

---
+++ src/presentation/pages/main/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './main-styles.scss'
+
+const Main: React.FC = () => {
+  return (
+    <div className={Styles.main}>
+        Hello
+    </div>
+  )
+}
+
+export default Main
