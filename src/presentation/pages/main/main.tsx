a8f449e74bed8f98c5b17ab20dc6bae38b9fb2b9
Bruno de Paula <bruno.bert.jj@gmail.com>
2021-05-21 16:51:49 -0300

    fix: react as external - outside bundle

--- src/presentation/pages/main/main.tsx
+++ src/presentation/pages/main/main.tsx
@@ -4,7 +4,11 @@ import Styles from './main-styles.scss'
 const Main: React.FC = () => {
   return (
     <div className={Styles.main}>
-        Hello
+      <header className={Styles.header}></header>
+      <form className={Styles.form}>
+          Hello LIvre Reload!
+      </form>
+      <footer className={Styles.footer}></footer>
     </div>
   )
 }
