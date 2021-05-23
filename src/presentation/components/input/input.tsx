17938e52530f276d46ae2376a1ee7248fb2f965b
Bruno de Paula <bruno.bert.jj@gmail.com>
2021-05-23 11:46:39 -0300

    feat: email and password state in login

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,16 +5,23 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
 
   /** hack - removes autofill input in Google Chrome */
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
   }
 
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
+
   const getStatus = (): string => {
-    return '&#x1F534;'
+    return '\u{1F534}'
   }
 
   const getTitle = (): string => {
@@ -23,7 +30,7 @@ const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input data-testid={props.name} {...props} readOnly onFocus={enableInput} onChange={handleChange}/>
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.inputStatus}>{getStatus()}</span>
     </div>
   )
